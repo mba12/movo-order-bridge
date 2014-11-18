4930cd1b8ac4cb8e0496051475122ef65791f7b8
Alex Bogartz <alex@jumpkick.pro>
2014-11-18 13:43:16 -0800

    formatted code.

--- app/Movo/Receipts/MailgunReceipts.php
+++ app/Movo/Receipts/MailgunReceipts.php
@@ -1,13 +1,15 @@
 <?php
 
 namespace Movo\Receipts;
+
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Mail;
+
 class MailgunReceipts extends Receipt implements ReceiptsInterface
 {
     public function send(array $data)
     {
-       $emailData=$this->createEmailData($data);
+        $emailData = $this->createEmailData($data);
         Mail::send('emails.receipt', array('data' => $emailData), function ($message) {
             $message->to(Input::get("email"), Input::get("billing-first-name") . ' ' . Input::get("billing-last-name"))->subject('Movo Order Confirmation')->from("orders@getmovo.com");
         });
