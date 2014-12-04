00d8c3fb5570c6458acfb33d2d2500cabc638eb3
Alex Bogartz <alex@jumpkick.pro>
2014-12-03 20:47:28 -0800

    fixed logout button

--- app/views/admin/header.blade.php
+++ app/views/admin/header.blade.php
@@ -5,9 +5,10 @@
            <div class="logo"></div>
         </a>
         <ul id="nav-links">
-            @if(Auth::check())
-            @endif
+            @if(Session::get('admin'))
                 <li><a href="/admin/logout" class="">logout</a></li>
+            @endif
+
             <li><a href="/admin/coupons" class="">coupons</a></li>
             <li><a href="/admin/orders" class="">orders</a></li>
             <li><a href="/admin" class="active">stats</a></li>
