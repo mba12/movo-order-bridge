27fa9e9b06b7a8bc175036f6ae208df626e447bc
Alex Bogartz <alex@jumpkick.pro>
2014-12-09 16:04:37 -0800

    test

--- app/views/order-form.blade.php
+++ app/views/order-form.blade.php
@@ -9,6 +9,7 @@
         {{Assets::css()}}
     </head>
 <body class="{{ $after3pm ? 'after3pm' : '' }}">
+test
         <script type="text/javascript">
             var TAX_TABLE=[];
             @foreach($stateTaxMethods as $method)
