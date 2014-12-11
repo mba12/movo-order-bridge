750a62744b5a38fa901b9151f37fcc837a822706
Ryan Hovland <ryanhovland@gmail.com>
2014-12-11 10:18:28 -0800

    css inc.

--- app/config/packages/stolz/assets/config.php
+++ app/config/packages/stolz/assets/config.php
@@ -100,7 +100,7 @@ return array(
     | Default: false
     */
 
-    'pipeline' => App::environment() == 'local' ? 0 : 18,
+    'pipeline' => App::environment() == 'local' ? 0 : 19,
 
     /*
     |--------------------------------------------------------------------------
