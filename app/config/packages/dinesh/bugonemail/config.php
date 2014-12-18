db1b4971417f0fd8ea945e745469e1075e6644e0
Ryan Hovland <ryanhovland@gmail.com>
2014-12-18 11:21:12 -0800

    added ryan's email to server error emailing.

--- app/config/packages/dinesh/bugonemail/config.php
+++ app/config/packages/dinesh/bugonemail/config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'project_name' => 'Movo Orders',
-    'notify_emails' => array('alex@jumpkick.pro','amelia@getmovo.com','michael@getmovo.com'),
+    'project_name' => 'Error on Movo production server!',
+    'notify_emails' => array('alex@jumpkick.pro','amelia@getmovo.com','michael@getmovo.com', 'ryan@jumpkick.pro'),
     'email_template' => "bugonemail::email.notifyException",
     'notify_environment' => array( 'production'),
     'prevent_exception' => array('Symfony\Component\HttpKernel\Exception\NotFoundHttpException'),
