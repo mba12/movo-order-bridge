d8594cccf14c1b12c8cce3b0120c528f5fedabba
Alex Bogartz <alex@jumpkick.pro>
2015-01-01 13:08:59 -0800

    'incrementing'

--- app/config/packages/stolz/assets/config.php
+++ app/config/packages/stolz/assets/config.php
@@ -99,7 +99,7 @@ return array(
     | Default: false
     */
 
-    'pipeline' => App::environment() == 'local' ? 0 : 32,
+    'pipeline' => App::environment() == 'local' ? 0 : 33,
 
     /*
     |--------------------------------------------------------------------------
