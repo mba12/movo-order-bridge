e806b4b76a925f8fdaa52c4ddaa072c12baf6d16
Alex Bogartz <alex@jumpkick.pro>
2015-01-13 14:35:24 -0800

    working on cert test

--- app/controllers/IngramController.php
+++ app/controllers/IngramController.php
@@ -11,6 +11,8 @@ class IngramController extends \BaseController {
 		$log = new Logger('ingram-inventory');
 		$log->pushHandler(new StreamHandler('../app/storage/logs/inventory.log', Logger::INFO));
 		$log->addInfo(json_encode(Input::all()));
+		$content =  View::make("ingram.track-inventory");
+		return Response::make($content, '200')->header('Content-Type', 'text/xml');
 	}
 
 	public function returns()
@@ -24,11 +26,15 @@ class IngramController extends \BaseController {
 		$log = new Logger('ingram-ship-advice');
 		$log->pushHandler(new StreamHandler('../app/storage/logs/ship-advice.log', Logger::INFO));
 		$log->addInfo(json_encode(Input::all()));
+		$content =  View::make("ingram.ship-advice");
+		return Response::make($content, '200')->header('Content-Type', 'text/xml');
 	}
 
 	public function orderStatus(){
 		$log = new Logger('ingram-order-status');
 		$log->pushHandler(new StreamHandler('../app/storage/logs/order-status.log', Logger::INFO));
 		$log->addInfo(json_encode(Input::all()));
+		$content =  View::make("ingram.order-status");
+		return Response::make($content, '200')->header('Content-Type', 'text/xml');
 	}
 }
