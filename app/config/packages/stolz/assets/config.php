c170e7bfbf30190d16b01922bf6ade8f65b66a06
Ryan Hovland <ryanhovland@gmail.com>
2015-01-22 15:39:42 -0800

    incrementing

--- app/config/packages/stolz/assets/config.php
+++ app/config/packages/stolz/assets/config.php
@@ -99,7 +99,7 @@ return array(
     | Default: false
     */
 
-    'pipeline' => App::environment() == 'local' ? 0 : 39,
+    'pipeline' => App::environment() == 'local' ? 0 : 40,
 
     /*
     |--------------------------------------------------------------------------
