a594dab7daef84b83758eb99a06d1af52b6aee24
Michael Ahern <michael@michaelahern.com>
2015-03-18 12:07:09 -0400

    changing date format

--- app/Movo/Shipping/IngramShipping.php
+++ app/Movo/Shipping/IngramShipping.php
@@ -139,7 +139,7 @@ class IngramShipping implements ShippingInterface
     public function generateXMLFromData(array $data)
     {
         $date = new \DateTime;
-        $date_str = date_format($date, 'Y-m-d');
+        $date_str = date_format($date, 'Ymd');
         $array = [
             'message' => [
                 'message-header' => [
