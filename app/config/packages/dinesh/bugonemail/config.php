4e1ca8350b228a25fde046d716cecc6751cd4cd6
Alex Bogartz <alex@jumpkick.pro>
2015-03-19 22:08:41 -0700

    updated email list.

--- app/config/packages/dinesh/bugonemail/config.php
+++ app/config/packages/dinesh/bugonemail/config.php
@@ -2,7 +2,7 @@
 
 return array(
     'project_name' => 'Error on Movo production server!',
-    'notify_emails' => array('alex@jumpkick.pro','amelia@getmovo.com','michael@getmovo.com', 'ryan@jumpkick.pro'),
+    'notify_emails' => array('amelia@getmovo.com','michael@getmovo.com'),
     'email_template' => "bugonemail::email.notifyException",
     'notify_environment' => array( 'production'),
     'prevent_exception' => array(
