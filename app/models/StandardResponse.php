2712aa162d050c220fed04d61f88109b418872a4
Michael Ahern <michael@michaelahern.com>
2015-03-23 18:10:17 -0400

    changing _ to - to fix bug

--- app/models/StandardResponse.php
+++ app/models/StandardResponse.php
@@ -21,30 +21,30 @@ class StandardResponse extends \Eloquent
     public function parseAndSaveData($xmlString)
     {
         Log::info("Incoming String: " . $xmlString);
-        $xml = new SimpleXMLElement($xmlString);
 
-        $testArray = $xml->message_id;
+        $xml = simplexml_load_string($xmlString);
+        //$xml = new SimpleXMLElement($xmlString);
+
+        $testArray = $xml->message-id;
         Log::info("Test: " . $testArray);
 
-        //$messageId = $xml->xpath('//message_id');
-        $messageId = $xml->message_id;
-        // $transactionName = $xml->xpath('//transaction_name');
-        $transactionName = $xml->transaction_name;
-        $partnerName = $xml->xpath('//partner_name');
-        $partnerPassword = $xml->xpath('//partner_password');
-        $sourceUrl = $xml->xpath('//source_url');
-        $responseRequest = $xml->xpath('//response_request');
-        $status_code = $xml->xpath('//status_code');
-        $status_description = $xml->xpath('//status_description');
+        $messageId = $xml->xpath('//message-id');
+        $transactionName = $xml->xpath('//transaction-name');
+        $partnerName = $xml->xpath('//partner-name');
+        $partnerPassword = $xml->xpath('//partner-password');
+        $sourceUrl = $xml->xpath('//source-url');
+        $responseRequest = $xml->xpath('//response-request');
+        $status_code = $xml->xpath('//status-code');
+        $status_description = $xml->xpath('//status-description');
         $comments = $xml->xpath('//comments');
-        $responseTimestamp = $xml->xpath('//response_timestamp');
+        $responseTimestamp = $xml->xpath('//response-timestamp');
         $fileName = $xml->xpath('//filename');
         $eventId = $xml->xpath('//eventID');
 
         $response = StandardResponse::create([
 
-            'message_id' => (String) $messageId,
-            'transaction_name' => (String) $transactionName,
+            'message_id' => (String) $messageId[0],
+            'transaction_name' => (String) $transactionName[0],
             'partner_name' => (String) $partnerName[0],
             'partner_password' => (String) $partnerPassword[0],
             'source_url' => (String) $sourceUrl[0],
