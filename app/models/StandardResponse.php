4ecd8b6da2da6fa26a52ac9439cef0b7dccf15de
Michael Ahern <michael@michaelahern.com>
2015-03-23 17:19:40 -0400

    another test

--- app/models/StandardResponse.php
+++ app/models/StandardResponse.php
@@ -22,18 +22,18 @@ class StandardResponse extends \Eloquent
     {
         Log::info("Incoming String: " . $xmlString);
         $xml = new SimpleXMLElement($xmlString);
-        $messageId = (String) $xml->xpath('//message_id'); //[0];
-        $transactionName = (String) $xml->xpath('//transaction_name'); //[0];
-        $partnerName = (String) $xml->xpath('//partner_name'); //[0];
-        $partnerPassword = (String) $xml->xpath('//partner_password'); //[0];
-        $sourceUrl = (String) $xml->xpath('//source_url'); //[0];
-        $responseRequest = (String) $xml->xpath('//response_request'); //[0];
-        $status_code = (String) $xml->xpath('//status_code'); //[0];
-        $status_description = (String) $xml->xpath('//status_description'); //[0];
-        $comments = (String) $xml->xpath('//comments'); //[0];
-        $responseTimestamp = (String) $xml->xpath('//response_timestamp'); //[0];
-        $fileName = (String) $xml->xpath('//filename'); //[0];
-        $eventId = (String) $xml->xpath('//eventID'); //[0];
+        $messageId = (String) $xml->xpath('//message_id')[0];
+        $transactionName = (String) $xml->xpath('//transaction_name')[0];
+        $partnerName = (String) $xml->xpath('//partner_name')[0];
+        $partnerPassword = (String) $xml->xpath('//partner_password')[0];
+        $sourceUrl = (String) $xml->xpath('//source_url')[0];
+        $responseRequest = (String) $xml->xpath('//response_request')[0];
+        $status_code = (int) $xml->xpath('//status_code')[0];
+        $status_description = (String) $xml->xpath('//status_description')[0];
+        $comments = (String) $xml->xpath('//comments')[0];
+        $responseTimestamp = (String) $xml->xpath('//response_timestamp')[0];
+        $fileName = (String) $xml->xpath('//filename')[0];
+        $eventId = (String) $xml->xpath('//eventID')[0];
 
         $response = StandardResponse::create([
 
