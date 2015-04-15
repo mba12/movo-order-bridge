954e9c318841a8c8ba9a7192581e7887d3fbc25b
Michael Ahern <michael@michaelahern.com>
2015-04-15 14:07:06 -0400

    adding checks on address fields

--- app/Movo/Receipts/Receipt.php
+++ app/Movo/Receipts/Receipt.php
@@ -16,9 +16,9 @@ class Receipt {
     public  function createEmailData($data)
     {
         $emailData['items'] = $data['items'];
-        $emailData['address1'] = $data['address1'];
-        $emailData['address2'] = $data['address2'];
-        $emailData['address3'] = $data['address3'];
+        $emailData['address1'] = (isset($data['address1']) && strlen($data['address1']) > 0)?$data['address1']:'';;
+        $emailData['address2'] = (isset($data['address2']) && strlen($data['address2']) > 0)?$data['address2']:'';;
+        $emailData['address3'] = (isset($data['address3']) && strlen($data['address3']) > 0)?$data['address3']:'';
         $emailData['address4'] = $data['address4'];
         $emailData['name'] = $data['name'];
         $emailData['total'] = Format::FormatStripeMoney($data['result']['amount']);
